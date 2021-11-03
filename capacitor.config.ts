ebddc1007b19c24e63b799ce1055cf6ae0ac22e3
TKhumalo <tkhumalo@aolc.co.za>
2021-11-03 09:03:49 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PaymentProcessingClient',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
